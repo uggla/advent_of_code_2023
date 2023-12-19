9e6877bb6b0a62b918bdf9f5a5e0bca61cc53583
Uggla <rene.ribaud@gmail.com>
2023-12-19 13:10:36 +0100

    feat: AOC day_13 solution 1

--- day_13/s1/src/main.rs
+++ day_13/s1/src/main.rs
@@ -1,12 +1,6 @@
-use itertools::Itertools;
-use std::ops::Add;
-
 use nom::{
-    branch::alt,
-    bytes::complete::{tag, take_until},
-    character::complete::{anychar, line_ending, multispace1, none_of, not_line_ending},
-    multi::{many0, many1, separated_list0, separated_list1},
-    sequence::terminated,
+    character::complete::{line_ending, none_of},
+    multi::{many1, separated_list1},
     *,
 };
 
@@ -35,7 +29,6 @@ fn parse_pattern(input: &str) -> IResult<&str, Vec<Vec<char>>> {
 fn parse_line(input: &str) -> IResult<&str, Vec<char>> {
     let (input, characters) = many1(none_of("\n"))(input)?;
     let (input, _) = line_ending(input)?;
-    // let characters = characters.to_string().chars().collect();
     Ok((input, characters))
 }
 
@@ -44,67 +37,10 @@ struct Data {
     patterns: Vec<Vec<Vec<char>>>,
 }
 
-impl Data {
-    // fn insert_row(&mut self, index: usize, new_row: Vec<char>) {
-    //     if index <= self.grid.len() {
-    //         self.grid.insert(index, new_row);
-    //     } else {
-    //         self.grid.push(new_row);
-    //     }
-    //     self.length_y += 1;
-    // }
-    //
-    // fn insert_column(&mut self, index: usize, new_value: char) {
-    //     for row in self.grid.iter_mut() {
-    //         if index <= row.len() {
-    //             row.insert(index, new_value);
-    //         } else {
-    //             row.push(new_value);
-    //         }
-    //     }
-    //     self.length_x += 1;
-    // }
-}
-
-#[derive(Debug, Eq, PartialEq, Clone, Copy, Ord, PartialOrd, Hash)]
-struct Coord {
-    x: isize,
-    y: isize,
-}
-
-impl From<(isize, isize)> for Coord {
-    fn from((x, y): (isize, isize)) -> Self {
-        Self { x, y }
-    }
-}
-
-impl Add<Coord> for Coord {
-    type Output = Coord;
-    fn add(self, rhs: Coord) -> Self::Output {
-        Self {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
-    }
-}
-
-fn print_text_map(coordinates: &[((usize, usize), char)], width: usize, height: usize) {
-    let mut grid = vec![vec!['.'; width]; height];
-
-    // Place the points on the grid
-    for &((x, y), v) in coordinates {
-        if x < width && y < height {
-            grid[y][x] = v; // Assuming the origin (0,0) is at the top-left corner
-        }
-    }
-
-    // Print the grid row by row
-    for row in grid {
-        for cell in row {
-            print!("{}", cell);
-        }
-        println!(); // Newline at the end of each row
-    }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum SymAxis {
+    X,
+    Y,
 }
 
 fn run(input: String) -> usize {
@@ -114,26 +50,17 @@ fn run(input: String) -> usize {
     let sym_y = data
         .patterns
         .iter()
-        .map(|p| {
-            p.windows(2)
-                .enumerate()
-                .filter_map(|y| if y.1[0] == y.1[1] { Some(y.0) } else { None })
-                .collect::<Vec<usize>>()
-        })
+        .map(|p| find_symetry_axis(p, SymAxis::Y))
         .collect::<Vec<_>>();
 
     let sym_x = data
         .patterns
         .iter()
         .map(|p| {
-            let columns = (0..p[0].len())
+            let pattern_transposed = (0..p[0].len())
                 .map(|x| p.iter().map(|y| y[x]).collect::<Vec<char>>())
                 .collect::<Vec<Vec<char>>>();
-            columns
-                .windows(2)
-                .enumerate()
-                .filter_map(|c| if c.1[0] == c.1[1] { Some(c.0) } else { None })
-                .collect::<Vec<usize>>()
+            find_symetry_axis(&pattern_transposed, SymAxis::X)
         })
         .collect::<Vec<_>>();
 
@@ -144,58 +71,48 @@ fn run(input: String) -> usize {
         .patterns
         .iter()
         .enumerate()
-        .map(|p| {
-            let lx = p.1[0].len();
-            let ly = p.1.len();
-
-            let xaxis = lx / 2;
-            let yaxis = ly / 2;
-
-            let bestx = sym_x[p.0]
-                .iter()
-                .map(|x| ((*x as isize - xaxis as isize).abs(), *x))
-                .min()
-                .unwrap_or((100, 0));
-
-            let besty = sym_y[p.0]
-                .iter()
-                .map(|y| ((*y as isize - yaxis as isize).abs(), *y))
-                .min()
-                .unwrap_or((100, 0));
-
-            dbg!(&bestx, &besty);
-
-            if bestx.0 < besty.0 {
-                bestx.1 + 1
-            } else {
-                (besty.1 + 1) * 100
-            }
+        .flat_map(|(i, _p)| {
+            let mut merged_axis = sym_y[i].clone();
+            merged_axis.append(&mut sym_x[i].clone());
+            merged_axis
+        })
+        .map(|(axis, i)| match axis {
+            SymAxis::X => i + 1,
+            SymAxis::Y => (i + 1) * 100,
         })
         .collect::<Vec<_>>();
 
-    dbg!(output.iter().sum::<usize>())
+    output.iter().sum::<usize>()
+}
 
-    // let insert_col_indices = (0..data.length_x)
-    //     .filter(|x| {
-    //         data.grid
-    //             .iter()
-    //             .map(|y| if y[*x] == '.' { Some('.') } else { None })
-    //             .all(|x| x == Some('.'))
-    //     })
-    //     .collect::<Vec<usize>>();
+fn find_symetry_axis(p: &Vec<Vec<char>>, axis: SymAxis) -> Vec<(SymAxis, usize)> {
+    p.windows(2)
+        .enumerate()
+        .filter_map(|y| {
+            if y.1[0] == y.1[1] {
+                let before = (0..=y.0)
+                    .rev()
+                    .flat_map(|i| p[i].to_vec())
+                    .collect::<Vec<_>>();
+                let after = ((y.0 + 1)..p.len())
+                    .flat_map(|i| p[i].to_vec())
+                    .collect::<Vec<_>>();
 
-    // println!("Map");
-    // print_text_map(
-    //     &data.patterns[1]
-    //         .iter()
-    //         .enumerate()
-    //         .flat_map(|(y, r)| r.iter().enumerate().map(move |(x, v)| ((x, y), *v)))
-    //         .collect::<Vec<((usize, usize), char)>>(),
-    //     9,
-    //     7,
-    // );
-    //
-    // todo!();
+                let length = match before.len() < after.len() {
+                    true => before.len(),
+                    false => after.len(),
+                };
+
+                if before[..length] == after[..length] {
+                    Some((axis, y.0))
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>()
 }
 
 fn main() {
@@ -238,19 +155,22 @@ mod tests {
             #####.##.
             ..##..###
             #....#..#
-
-            ##..#.#..##
-            ###...#....
-            ##.##.##...
-            ....###..##
-            ####..###..
-            ##.####....
-            ...####.###
-            ###...##...
-            ##...#..###
-            ##...#.....
-            .##..##.###
-            "
+            " //
+              // ###...###..
+              // ###...###..
+              // #.#.#.#...#
+              // ...#..##.#.
+              // ...#..#....
+              // .###.#.###.
+              // #..##.###..
+              // ..#..#.#.##
+              // ..#..#.#.##
+              // #..##.###..
+              // .###.#.###.
+              // ...#..#....
+              // ...#..##.#.
+              // #.#.#.#.#.#
+              // ###...###..
         )));
 
         dbg!(&input);
